5e9e23e3c1e5d146306539412ddfd16ed7b9cdcb
Su La Pyae Phyoe <slpp.sulapyaephyoe@gmail.com>
2024-07-19 14:27:44 +0900

    new form added

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "articles#index"
-  get "/articles", to: "articles#index"
-end
+  resources :articles
+  # get "/articles", to: "articles#index"
+end   
